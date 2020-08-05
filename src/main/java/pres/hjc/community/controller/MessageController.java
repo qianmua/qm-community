d7fdd4a7ffe93325836c9ca3b4f7a14be40a46d2
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-08-05 12:44:35 +0800

    私信展示

---
+++ src/main/java/pres/hjc/community/controller/MessageController.java
@@ -0,0 +1,96 @@
+package pres.hjc.community.controller;
+
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pres.hjc.community.dto.PageDTO;
+import pres.hjc.community.entity.UserPO;
+import pres.hjc.community.service.MessageService;
+import pres.hjc.community.service.UserService;
+import pres.hjc.community.tools.HostHolder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/8/5  12:15
+ * @description :
+ */
+@Controller
+@RequestMapping("/message")
+public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @Autowired
+    HostHolder hostHolder;
+
+    @Autowired
+    UserService userService;
+
+    /*@GetMapping("/letter")
+    public String getLetterView(){
+        return "site/letter";
+    }*/
+
+    /**
+     * 私信 列表
+     * @param model
+     * @param pageDTO
+     * @return
+     */
+    @GetMapping("/letter/list")
+    public String getLetterList(Model model , PageDTO pageDTO){
+
+        UserPO po = hostHolder.getUsersPO();
+
+        pageDTO.setLimit(5);
+        /*message/ */
+        pageDTO.setPath("/letter/list");
+        pageDTO.setRows(messageService.selectConversationCount(po.getId()));
+
+        // 会话列表
+        // list<Message>
+        val pos = messageService.selectConversations(po.getId(), pageDTO.getOffset(), pageDTO.getLimit());
+
+        // 私信 数量
+        ArrayList<Map<String, Object>> cov = new ArrayList<>();
+
+        pos.forEach(v1 -> {
+            HashMap<String, Object> hashMap = new HashMap<>();
+
+            hashMap.put("conversations" , v1);
+            // 未读消息
+            hashMap.put("unreadCount" , messageService.selectLetterUnreadCount(po.getId() , v1.getConversationId()));
+
+            //包含消息数
+            // 总消息数
+            hashMap.put("letterCount" , messageService.selectLetterCount(v1.getConversationId()));
+
+            // 用户信息
+            int targetId = po.getId() == v1.getFromId() ? v1.getToId() : v1.getFromId();
+            hashMap.put("target" , userService.selectById(targetId));
+
+            cov.add(hashMap);
+
+        });
+
+
+        // 未读消息 总数
+        val unreadCount = messageService.selectLetterUnreadCount(po.getId(), null);
+        model.addAttribute("letterUnreadCount" , unreadCount);
+        model.addAttribute("conversation" , cov);
+
+        return "site/letter";
+    }
+
+
+}
---
+++ src/main/java/pres/hjc/community/service/impl/MessageServiceImpl.java
@@ -0,0 +1,82 @@
+package pres.hjc.community.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import pres.hjc.community.dao.MessageMapper;
+import pres.hjc.community.entity.MessagePO;
+import pres.hjc.community.service.MessageService;
+
+import java.util.List;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/8/5  12:17
+ * @description :
+ */
+@Service
+@Primary
+public class MessageServiceImpl implements MessageService {
+
+    @Autowired
+    private MessageMapper messageMapper;
+
+
+
+    @Override
+    public List<MessagePO> selectConversations(int userId, int offset, int limit) {
+        return messageMapper.selectConversations(userId, offset, limit);
+    }
+
+    @Override
+    public int selectConversationCount(int userId) {
+        return messageMapper.selectConversationCount(userId);
+    }
+
+    @Override
+    public List<MessagePO> selectLetters(String conversationId, int offset, int limit) {
+        return messageMapper.selectLetters(conversationId, offset, limit);
+    }
+
+    @Override
+    public int selectLetterCount(String conversationId) {
+        return messageMapper.selectLetterCount(conversationId);
+    }
+
+    @Override
+    public int selectLetterUnreadCount(int userId, String conversationId) {
+        return messageMapper.selectNoticeUnreadCount(userId, conversationId);
+    }
+
+    @Override
+    public int insertMessage(MessagePO message) {
+        return messageMapper.insertMessage(message);
+    }
+
+    @Override
+    public int updateStatus(List<Integer> ids, int status) {
+        return messageMapper.updateStatus(ids, status);
+    }
+
+    @Override
+    public MessagePO selectLatestNotice(int userId, String topic) {
+        return messageMapper.selectLatestNotice(userId, topic);
+    }
+
+    @Override
+    public int selectNoticeCount(int userId, String topic) {
+        return messageMapper.selectNoticeCount(userId, topic);
+    }
+
+    @Override
+    public int selectNoticeUnreadCount(int userId, String topic) {
+        return messageMapper.selectNoticeUnreadCount(userId, topic);
+    }
+
+    @Override
+    public List<MessagePO> selectNotices(int userId, String topic, int offset, int limit) {
+        return null;
+    }
+}
